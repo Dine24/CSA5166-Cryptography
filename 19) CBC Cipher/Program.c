34496a2140c216a543c691da8f70769e355efc8a
Dinesha G <120408570+Dine24@users.noreply.github.com>
2026-06-23 18:42:34 +0530

    Create Program.c

---
+++ 19) CBC Cipher/Program.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    printf("CBC Mode Analysis\n\n");
+
+    printf("For Security  : Use 3DES\n");
+    printf("For Performance : Use DES\n\n");
+
+    printf("3DES provides higher security.\n");
+    printf("DES provides better speed.\n");
+
+    getch();
+    return 0;
+}
